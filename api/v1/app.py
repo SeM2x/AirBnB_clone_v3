ad8648df0e041c7704328f0d3a5d64922a41e37b
SeM2x <houssem135.302@gmail.com>
2024-08-03 21:28:22 +0100

    add teardown method

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-
-"""
-    App
-"""
+"""Creates a flask app"""
 from os import getenv
 from flask import Flask
 from models import storage
-from api.v1.views import app_view
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
-app.register_blueprint(app_view)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown():
+    """method to handle teardown"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST','0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT','5000'))
-    app.run(host=HOST, port=PORT,threaded=True)
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = getenv('HBNB_API_PORT', '5000')
+    app.run(host=HOST, port=PORT, threaded=True)
