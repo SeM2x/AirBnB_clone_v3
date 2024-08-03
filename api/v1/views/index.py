bf63c2496008c85b32bfb38af41aad1783e01ae5
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-08-03 21:56:50 +0100

    all

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Creates app views"""
+from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -10,3 +11,19 @@ def api_status():
     """
     response = {'status': "OK"}
     return jsonify(response)
+
+@app_view.route('/stats')
+def get_stats():
+    """
+    
+    """
+    stats = {
+        "amenities": storage.count('Amenity'), 
+        "cities": storage.count('City'), 
+        "places": storage.count('Place'), 
+        "reviews": storage.count('Review'), 
+        "states": storage.count('State'), 
+        "users": storage.count('User')
+    }
+
+    return jsonify(stats)
