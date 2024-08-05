9c90ece7dd869ba4069ca1b5fee2ac694dcd8d8c
SeM2x <houssem135.302@gmail.com>
2024-08-05 21:48:51 +0100

    testing

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,7 +8,6 @@ from api.v1.views.index import *
 from api.v1.views  import states
 from api.v1.views import cities
 from api.v1.views import amenities
-from api.v1.views import user
 from api.v1.views import places
 from api.v1.views import places_amenities
 from api.v1.views import users
